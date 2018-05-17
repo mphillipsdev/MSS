db96c9e3f738483d4897e8ce76a16d40ebe20330
mphillipsdev <mphillips.dev@gmail.com>
2018-05-17 13:50:56 -0400

    Removed Sysout. -MP

--- DevOps/src/main/java/com/revature/Application.java
+++ DevOps/src/main/java/com/revature/Application.java
@@ -8,7 +8,7 @@ public class Application
 
 	public static void main(String[] args)
 	{
-		System.out.println("Running!");
+		// System.out.println("Running!");
 		SpringApplication.run(Application.class);
 	}
 
